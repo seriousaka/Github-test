db653e32787441d393c9b55527682e996e8f35d2
seriousaka <azamjonbro@gmail.com>
2025-05-26 17:46:11 +0500

    added second folder

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,47 @@
 <template>
-  <div>Salom bolalar</div>
-  <header>
-    <a href="/">Logo</a>
-    <nav>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/">Home</a>
-        </li>
-      </ul>
-    </nav>
-  </header>
+  <div @click="onclick($event)">{{ message }}</div> 
+  <!-- <!-- <div class="menubox" v-if="showmenu" ></div> -->
+  <!-- <div class="menubox" v-else-if="showmenu" ></div> -->
+  <!-- <div class="menubox" v-else ></div> -->  
+  <!-- bu agar qiymat v-if orqali oynadan yashirilsa element render bolmasdan oldin yoq bolib qoladi  -->
+  <!-- agar bizga render bolgan holda oynadan yashirilish kerak bolsa v-showdan foydalaniladi -->
+  <div class="menubox" v-show="showmenu" ></div>
 </template>
 
 
 <script>
 // bu yerga har doim biz scriptlarni yozamiz hatto import qilmoqchi boslak ham
-export default {};
+export default {
+  data(){
+    return{
+      message: 'Salom Vue!',
+      showmenu: false
+    }
+  },
+  methods:{
+    onclick(event){
+      console.log(event);
+      // bu murakkab va hato usul
+      // if(this.showmenu){
+      //   this.showmenu = false;
+      // }
+      // else{
+      //   this.showmenu = true;
+      // }
+      this.showmenu=!this.showmenu; // bu oddiy va to'g'ri usul
+    }
+  }
+};
 </script>
-
-
-
-
 
 <style>
 div {
   color: red;
 }
-header{
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 10px 120px;
-}
-nav ul{
-  display: flex;
-  list-style: none;
-  gap: 20px;
-}
+.menubox {
+  width: 100px;
+  height: 100px;
+  background-color: blue;
+  margin: 10px;
+  border: 1px solid black;}
 </style>
