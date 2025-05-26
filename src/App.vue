4e2732cf0d69f00858cfd07012c9e1fb12221f6f
Shoboev <shoboevmubosher@gmail.com>
2025-05-26 18:00:31 +0500

    added

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
-  <div @click="onclick($event)">{{ message }}</div> 
+  <div @click="onclick($event)">{{ message }}</div>
   <!-- <!-- <div class="menubox" v-if="showmenu" ></div> -->
   <!-- <div class="menubox" v-else-if="showmenu" ></div> -->
-  <!-- <div class="menubox" v-else ></div> -->  
+  <!-- <div class="menubox" v-else ></div> -->
   <!-- bu agar qiymat v-if orqali oynadan yashirilsa element render bolmasdan oldin yoq bolib qoladi  -->
   <!-- agar bizga render bolgan holda oynadan yashirilish kerak bolsa v-showdan foydalaniladi -->
-  <div class="menubox" v-show="showmenu" ></div>
+  <div class="menubox" v-show="showmenu"></div>
 </template>
 
 
 <script>
 // bu yerga har doim biz scriptlarni yozamiz hatto import qilmoqchi boslak ham
 export default {
-  data(){
-    return{
+  data() {
+    return {
       message: 'Salom Vue!',
       showmenu: false
     }
   },
-  methods:{
-    onclick(event){
+  methods: {
+    onclick(event) {
       console.log(event);
       // bu murakkab va hato usul
       // if(this.showmenu){
@@ -28,7 +28,7 @@ export default {
       // else{
       //   this.showmenu = true;
       // }
-      this.showmenu=!this.showmenu; // bu oddiy va to'g'ri usul
+      this.showmenu = !this.showmenu; // bu oddiy va to'g'ri usul
     }
   }
 };
@@ -36,12 +36,20 @@ export default {
 
 <style>
 div {
+  width: 100hv;
+  height: 100hv;
   color: red;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: bisque;
 }
+
 .menubox {
   width: 100px;
   height: 100px;
   background-color: blue;
   margin: 10px;
-  border: 1px solid black;}
+  border: 1px solid black;
+}
 </style>
